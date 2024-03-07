88f0f06971b41996268c82d56fd9babd33f1eeb0
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-03-07 14:17:05 +0200

    update models

--- recipe/models.py
+++ recipe/models.py
@@ -2,26 +2,49 @@ from django.db import models
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=100)
-    quantity = models.FloatField(max_length=200)
-    optional = models.BooleanField(default = False)
-    unit = models.CharField(max_length=50)
+    UNITS = [
+            ('Grams', 'Grams'),
+            ('Kilograms', 'Kilograms'),
+            ('Milliliters', 'Milliliters'),
+            ('Liters', 'Liters'),
+            ('Teaspoons', 'Teaspoons'),
+            ('Tablespoons', 'Tablespoons'),
+            ('Cups', 'Cups'),
+            ('Ounces', 'Ounces'),
+            ('Pounds', 'Pounds'),
+            ('Pieces', 'Pieces'),
+            ('Slices', 'Slices'),
+            ('Cloves', 'Cloves'),
+            ('Pinches', 'Pinches'),
+            ('Handfuls', 'Handfuls'),
+            ('Drops', 'Drops'),
+    ]
+    name = models.CharField(max_length=70)
+    unit = models.CharField(max_length=20,choices=UNITS)
+    quantity = models.FloatField(default=5)
+    optional = models.BooleanField(default = False)         
 
     def __str__(self):
-        return self.name
+        return f'{self.quantity} {self.unit} {self.name}'
 class Recipe(models.Model):
+    CATEGORY_CHOICES = [
+        ('dessert', 'Dessert'),
+        ('salad', 'Salad'),
+        ('appetizer', 'Appetizer'),
+        ('main_course', 'Main Course'),
+        ('side_dish', 'Side Dish'),
+        ('beverage', 'Beverage')
+    ]
     name = models.CharField(max_length=100)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     picture = models.ImageField(upload_to = "media/",blank=False,null=False)
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=50,choices = CATEGORY_CHOICES)
     time_cooked = models.IntegerField()
-    ingredients = models.ManyToManyField(Ingredient,related_name='recipes')
+    ingredients = models.ManyToManyField(Ingredient,related_name="recipe")
 
     def __str__(self):
         return self.name
-
-
 
      
 
