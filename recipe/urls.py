f11ba2978ad92821bf4e4c94bc158f6fe6a09edc
MaiEl5odery <105084125+MaiElkhodery@users.noreply.github.com>
2024-03-14 15:11:05 +0200

    modify add_ingredient path name of add_recipe

--- recipe/urls.py
+++ recipe/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('recipe/<int:recipe_id>', views.recipe, name = 'recipe_detail'),
-    path('add_recipe/',views.add_recipe,name='add recipe'),
-    path('add_ingredients/',views.add_recipe_ingredients,name='add ingredients')
+    path('add_recipe/',views.add_recipe,name='add_recipe'),
+    path('add_recipe/<int:recipe_id>/add_ingredients/',views.add_recipe_ingredients,name='add_ingredients')
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
